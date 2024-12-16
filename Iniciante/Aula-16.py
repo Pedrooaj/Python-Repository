9620d325cbe8229789468f95be3c3a88225ac682
pedrooaj <pedroantonio5735@gmail.com>
2024-12-16 12:57:30 -0300

    update

---
+++ Iniciante/Aula-16.py
@@ -0,0 +1,17 @@
+"""
+Operadores Lógicos
+Python Vs Javascript
+and     |     &&
+or      |     ||
+not     |     !
+"""
+
+entrada = input('[E]ntrar [S]air: ')
+senha_digitada = input('Senha: ')
+
+
+senha_permitida = '123456'
+if entrada == 'E' and senha_digitada == senha_permitida:
+    print('Entrar')
+else:
+    print("Sair")
