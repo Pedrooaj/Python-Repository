17cf6225616d31c37390651df9468b14b1e65cc1
pedrooaj <pedroantonio5735@gmail.com>
2024-12-16 13:57:04 -0300

    update

---
+++ Iniciante/Aula-17.py
@@ -0,0 +1,13 @@
+"""
+Strings em python são interaveis
+pedro
+['p','e','d','r','o']
+"""
+
+nome = "pedro"
+
+# in esta incluso
+print("p" in nome)
+print(10 * "-")
+# not in 
+print("p" not in nome)
