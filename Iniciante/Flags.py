ef2e138f601715bfba591e8ca9369cb81c07b783
pedrooaj <pedroantonio5735@gmail.com>
2024-12-19 16:26:05 -0300

    update

--- Iniciante/Flags.py
+++ Iniciante/Flags.py
@@ -1,6 +1,19 @@
-condicao = True
+"""
+is 
+"""
 
+condicao = False
+passou_no_if = None
 if condicao: 
+    passou_no_if = True
     print("Faça algo")
 else:
     print("Não faça algo")
+
+
+
+
+if passou_no_if is not None:
+    print("Passou no if")
+else:
+    print("Não passou no if")
