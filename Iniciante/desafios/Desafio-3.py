c8bfbd89f44335006e58f5e99eac815b8c49b8bd
pedrooaj <pedroantonio5735@gmail.com>
2024-12-26 11:55:38 -0300

    update

--- Iniciante/desafios/Desafio-3.py
+++ Iniciante/desafios/Desafio-3.py
@@ -1,8 +1,16 @@
-nome = input("Digite seu nome: \n")
 
-if len(nome) <= 4:
-    print("Seu nome e muito curto")
-elif len(nome) > 4 and len(nome) <= 6:
-    print("Seu nome e normal")
-else:
-    print("Seu nome e muito grande")
+
+while True:
+    nome = input("Digite seu nome: \n")
+    if nome:
+        if len(nome) <= 4:
+            print("Seu nome e muito curto")
+            break
+        elif len(nome) > 4 and len(nome) <= 6:
+            print("Seu nome e normal")
+            break
+        else:
+            print("Seu nome e muito grande")
+            break
+    else:
+        print("Por favor, digite seu nome")
