3751ce8acfa90fb4ed86da8e66d290704c417bf1
pedrooaj <pedroantonio5735@gmail.com>
2025-01-09 16:49:27 -0300

    update

--- Iniciante/desafios/Palavra-Secreta.py
+++ Iniciante/desafios/Palavra-Secreta.py
@@ -1,15 +1,17 @@
 import requests
+import os
 print("Jogo da palavra secreta")
 print(30 * "=")
 res = requests.get("https://api.dicionario-aberto.net/random").json()
 secret_word = res["word"].lower()
 letters = ""
+attemps_number = 0
 
 
 
 while True:
     letter = input("Digite uma letra: ").lower()
-    
+    attemps_number += 1
     if len(letter) > 1:
         print("Digite apenas uma letra")
         continue
@@ -17,12 +19,26 @@ while True:
     if letter in secret_word:
         letters += letter
     
+    current_word = ""
+    
     for letter_secret in secret_word:
 
         if letter_secret in letters:
-            print(letter_secret)
+            current_word += letter_secret
         else:
-            print("*")
+             current_word += "*"
+    
+    print("Palavra formada: ", current_word)
+    
+    if current_word == secret_word:
+        print(f"Você ganhou. A palavra secreta era: {secret_word}")
+        res = requests.get("https://api.dicionario-aberto.net/random").json()
+        secret_word = res["word"].lower()
+        letters = ""
+        attemps_number = 0
+        os.system("cls")
+        
+    
 
         
             
