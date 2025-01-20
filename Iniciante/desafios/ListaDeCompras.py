82a8c577a9a9e3a9ae3884cd86bf3169ebe15db9
pedrooaj <pedroantonio5735@gmail.com>
2025-01-20 14:22:54 -0300

    update

--- Iniciante/desafios/ListaDeCompras.py
+++ Iniciante/desafios/ListaDeCompras.py
@@ -2,13 +2,27 @@ lista = []
 
 while True: 
     resposta = input("Lista de Compras\n[i]nserir [a]pagar [l]istar: ").lower()
-    
+    print("-" * 40)
     if resposta == 'i':
         lista.append(input("Digite o item que deseja adicionar: "))
+        print("-" * 40)
     elif resposta == 'a':
-        ...
+
+        try:
+            indice = int(input("Escolha um indice para apagar: "))
+            lista.pop(indice)
+            print("Item deletado com sucesso")  
+            print("-" * 40)
+        except:
+          print('O indice deve ser um número')
+          print("-" * 40)
     elif resposta == 'l':
-        print("Listando o carrinho de compras")
-        for i in range(len(lista)):
-            print(f"{i} {lista[i]}")
+        if len(lista) > 0:
+            print("Listando o carrinho de compras")
+            print("-" * 40)
+            for i in range(len(lista)):
+                print(f"{i} {lista[i]}")
+        else:
+            print("Nada pra listar")
+        print("-" * 40)
     
